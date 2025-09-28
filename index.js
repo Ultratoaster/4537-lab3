a630686936e0e36b1c2b4939743a8973a062330f
Ben Henry <bhenry18@my.bcit.ca>
2025-09-28 12:50:52 -0700

    casing fixes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Lab 3 API - Object-Oriented Design
 // Author: Ben Henry
 
-const HttpServer = require('./HttpServer');
+const HttpServer = require('./httpServer');
 const DateHandler = require('./handlers/dateHandler');
 const FileHandler = require('./handlers/fileHandler');
 
